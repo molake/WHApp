edcf13d5310068520b9d0ad9d2ec2efac2c197c7
Hao <hao.d.wu@accenture.com>
2020-01-10 18:04:55 +0800

    Import module

--- Example/Pods/Target Support Files/WHApp/WHApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WHApp : NSObject
-@end
-@implementation PodsDummy_WHApp
-@end
---
+++ WHApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WHApp
+//
+//  Created by wu, hao on 2020/1/10.
+//  Copyright © 2020 wuhao. All rights reserved.
+//
+
+import UIKit
+import WHUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
